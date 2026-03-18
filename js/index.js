c5a0be61783a641e3ea5be9d104b8b8d50f7860d
gykim_kr <k_bright_st@pusan.ac.kr>
2026-03-18 16:08:56 +0900

    feat : TIL 삭제 버튼 누를 시 confirm alert 나오도록 수정

--- js/index.js
+++ js/index.js
@@ -73,7 +73,9 @@ function createTilItem(date, title, content) {
   });
 
   deleteButton.addEventListener("click", function () {
-    tilItem.remove();
+    if (confirm("정말 삭제하시겠습니까?")) {
+      tilItem.remove();
+    }
   });
 
   actionContainer.append(editButton, deleteButton);
